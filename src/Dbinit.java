5633801578d1d3b3725b0beb47956daa875245b2
AkilPrasath <akilprasathr@gmail.com>
2021-01-17 21:27:34 +0530

    akil init commit

--- src/Dbinit.java
+++ src/Dbinit.java
@@ -3,8 +3,8 @@ public class Dbinit {
 	public static Connection connection;
 	public static Connection getConnection() {
 		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			connection = DriverManager.getConnection("jdbc:mysql://localhost/krishconnect","root","");
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection("jdbc:mysql://localhost/krishconnect-web","root","");
 			
 		}catch(Exception e) {
 			System.out.println(e);
